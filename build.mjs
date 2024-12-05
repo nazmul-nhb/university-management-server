c4978346e0d81f28644ee63f6100d27b6f78e18d
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-12-05 15:58:05 +0600

    updated build file

--- build.mjs
+++ build.mjs
@@ -3,6 +3,7 @@ import { execa } from 'execa';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import progressEstimator from 'progress-estimator';
+import { globby } from 'globby';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,20 +11,57 @@ const __dirname = dirname(__filename);
 // Set up progress-estimator cache
 const estimator = progressEstimator({ storagePath: join(__dirname, '.estimator') });
 
+// Function to determine icon based on file extension
+const getFileIcon = (filePath) => {
+    const ext = filePath.split('.').pop()?.toLowerCase();
+    return ext === 'js' ? '📄' : '📁'; // Add other icons if needed
+};
+
 (async () => {
     const startTime = performance.now();
 
     try {
-        await estimator(
-            execa('rimraf', ['dist']).then(() => execa('tsc', [], { stdio: 'inherit' })),
-            chalk.yellow('Building Your Express Application...')
+        // Clean and Build
+        await estimator(execa('rimraf', ['dist']).then(() => execa('tsc', [], { stdio: 'inherit' })),
+            chalk.yellowBright('Building Your Express Application...')
         );
 
-        const endTime = performance.now();
+        // Gather Transformed Files
+        const outputFiles = await globby(['dist/**/*'], { stats: true });
 
+        // Log Transformed Files
+        console.info(chalk.green('\n✓ Transformed Files:'));
+
+        let totalSize = 0;
+
+        const rows = outputFiles.map(({ path, stats }) => {
+            const sizeInKB = (stats?.size || 0) / 1024;
+
+            totalSize += sizeInKB;
+
+            const fileIcon = getFileIcon(path);
+            return [
+                chalk.yellow(`${fileIcon} ${path}`),
+                chalk.cyan(`${sizeInKB.toFixed(2)} kB`),
+            ];
+        });
+
+        const columnWidth = 80;
+
+        rows.forEach(([left, right]) => {
+            console.info(`${left.padEnd(columnWidth)}${right}`);
+        });
+
+        // Log Total Size and Build Time
+        const totalSizeInKB = totalSize.toFixed(2);
+
+        const endTime = performance.now();
+        
         const buildTime = ((endTime - startTime) / 1000).toFixed(2);
 
-        console.info(chalk.green(`✓ Application was built in ${chalk.redBright(buildTime)} seconds!`));
+        console.info(chalk.green(`\n✓ Total Size: ${chalk.blueBright.bold(totalSizeInKB)} kB`));
+
+        console.info(chalk.green(`\n✓ Application was built in ${chalk.blueBright.bold(buildTime)} seconds!`));
     } catch (error) {
         console.error(chalk.red('🛑 Build Failed!'), error);
         process.exit(1);
